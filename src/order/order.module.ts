7c9bf2707e5528690e030ff3bc082865b58e8449
N1GH7M4R3 <yordanbb98@gmail.com>
2024-07-25 23:19:01 -0400

    missing userService import

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, UserService],
   imports: [PrismaModule],
 })
 export class OrderModule {}
