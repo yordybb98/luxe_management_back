2cf323b606d59de2e896481f6881fb0c3dad909e
yordybb98 <yordanbb98@gmail.com>
2024-07-27 09:29:02 -0400

    testing cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || ['http://localhost:3000', 'http://localhost:4000'].includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
