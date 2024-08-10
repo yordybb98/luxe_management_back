9424ba6c788b2a5e025220cba8a4ed89545a65a2
yordybb98 <yordanbb98@gmail.com>
2024-08-10 11:24:43 -0400

    Increasing token life

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -2,6 +2,6 @@ export const jwtConstants = {
   // secret: process.env.JWT_SECRET, TODO
   secret: 'THIS IS JUTS FOR TEST',
 
-  // expeires: process.env.JWT_EXPIRES_IN, TODO
-  expiresIn: '60s',
+  // expires: process.env.JWT_EXPIRES_IN, TODO
+  expiresIn: '30m',
 };
