3a5380005ee285d2c2403425496414739c8c470a
yordybb98 <yordanbb98@gmail.com>
2024-09-28 16:19:54 -0400

    stage controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { OrderModule } from './order/order.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { RoleModule } from './role/role.module';
+import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     OrderModule,
     UserModule,
     AuthModule,
-    RoleModule /* 
+    RoleModule,
+    CommonModule /* 
     ProjectModule,
     ClientModule,
     DepartmentModule,
---
+++ src/common/common.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Request } from '@nestjs/common';
+import { authenticateFromOdoo, getOdooStages } from 'src/order/odooImport/api';
+import { Stage } from './types/stage';
+
+@Controller('')
+export class CommonController {
+  constructor() {}
+
+  @Get('stages')
+  async getStages(@Request() req) {
+    const uid = await authenticateFromOdoo();
+    const stages = await getOdooStages(uid);
+
+    const uniqueStages = [];
+
+    const seenNames = new Set();
+    for (const stage of stages as Stage[]) {
+      if (!seenNames.has(stage.name)) {
+        seenNames.add(stage.name);
+        uniqueStages.push(stage);
+      }
+    }
+
+    console.log(uniqueStages);
+
+    return uniqueStages;
+  }
+}
