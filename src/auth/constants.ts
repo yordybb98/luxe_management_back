9074313706a6a0f6082e4a4482b2ab49703ab123
yordybb98 <yordanbb98@gmail.com>
2024-10-23 14:55:28 -0400

    token expires

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,7 +1,5 @@
 export const jwtConstants = {
-  // secret: process.env.JWT_SECRET, TODO
-  secret: 'THIS IS JUTS FOR TEST',
+  secret: process.env.JWT_SECRET,
 
-  // expires: process.env.JWT_EXPIRES_IN, TODO
-  expiresIn: '8h',
+  expires: process.env.JWT_EXPIRES_IN,
 };
