abf9763bd0b974e3601f7ad06a651e21fc1cbd80
yordybb98 <yordanbb98@gmail.com>
2024-11-02 15:16:51 -0400

    Duplicated code

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -40,37 +40,4 @@ export class CommonController {
     const teams = await getOdooTeams(uid);
     return teams;
   }
-
-  @Post('image')
-  async uploadFile(@Req() req: Request, @Body() data) {
-    console.log({ data });
-    // Asegúrate de configurar el tamaño máximo en el lado del cliente y el límite de carga del servidor para evitar problemas de tamaño.
-    if (!data.file) {
-      throw new BadRequestException('No se ha proporcionado ningún archivo.');
-    }
-
-    const buffer = data.file.buffer; // Buffer del archivo recibido directamente desde la solicitud.
-    const dynamicPath = path.join('\\LUXE_BACKUP\\Luxe Files', `Folder`);
-    const uniqueFilename = `file-${Date.now()}.jpg`;
-    const outputPath = path.join(dynamicPath, uniqueFilename);
-
-    try {
-      // Verifica que el directorio exista o créalo
-      if (!existsSync(dynamicPath)) {
-        mkdirSync(dynamicPath, { recursive: true });
-      }
-
-      // Procesa y guarda la imagen comprimida
-      await sharp(buffer)
-        .resize({ width: 800, height: 800, fit: sharp.fit.inside })
-        .toFormat('jpeg', { quality: 80 })
-        .toFile(outputPath);
-
-      console.log('Archivo comprimido y guardado:', outputPath);
-      return { url: uniqueFilename };
-    } catch (error) {
-      console.error('Error al procesar la imagen:', error);
-      throw new BadRequestException('No se pudo procesar la imagen.');
-    }
-  }
 }
