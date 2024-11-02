7216228653f87478a320066dd14b52a3c31a66cd
yordybb98 <yordanbb98@gmail.com>
2024-11-02 16:33:10 -0400

    Removing unused code

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,26 +1,10 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Post,
-  Query,
-  Req,
-  Request,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, Get, Query, Request } from '@nestjs/common';
 import {
   authenticateFromOdoo,
   getOdooStages,
   getOdooTeams,
 } from 'src/order/odooImport/api';
 import { Stage } from './types/stage';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { multerOptions } from 'src/utils/multer';
-import path from 'path';
-import { existsSync, mkdirSync } from 'fs';
-import sharp from 'sharp';
 
 @Controller('')
 export class CommonController {
