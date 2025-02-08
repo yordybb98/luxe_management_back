7e964d756a186141bf810fd316c07870ebcf6e3f
Yordan Beltran <yordanbb98@gmail.com>
2025-02-08 12:38:38 -0500

    Employees endpoint

--- src/odoo/odoo.controller.ts
+++ src/odoo/odoo.controller.ts
@@ -1,15 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OdooService } from './odoo.service';
 import { Public } from 'src/common/guards/public.guard';
 
+@Public()
 @Controller('odoo')
 export class OdooController {
   constructor(private readonly odooService: OdooService) {}
 
-  @Public()
   @Get()
   async getVersion() {
     const odooVersion = await this.odooService.getVersion();
     return { odooVersion };
+  }
+  @Get('authenticate')
+  async authenticate() {
+    const uid = await this.odooService.authenticate();
+    return { uid };
+  }
+
+  @Get('employees')
+  async getEmployees() {
+    const uid = await this.odooService.authenticate();
+    const employees = await this.odooService.getEmployees(uid);
+    return { employees };
+  }
+
+  @Post('attendance')
+  async attendance(@Body() data: any) {
+    const uid = await this.odooService.authenticate();
+
+    const employee_id = 1;
+    const checkIn = '';
+    const checkOut = '';
+
+    const result = await this.odooService.createOdooAttendance(
+      uid,
+      employee_id,
+      checkIn,
+      checkOut,
+    );
+    return { result };
   }
 }
--- src/odoo/odoo.service.ts
+++ src/odoo/odoo.service.ts
@@ -46,4 +46,75 @@ export class OdooService {
       );
     });
   };
+
+  createOdooAttendance = async (
+    uid: number,
+    employeeId: number,
+    checkIn: string,
+    checkOut: string,
+  ): Promise<any> => {
+    try {
+      const record = await new Promise((resolve, reject) => {
+        this.modelsClient.methodCall(
+          'execute_kw', // Odoo method name
+          [
+            this.db, // Database name
+            uid, // User session ID
+            this.password, // Password
+            'hr.attendance', // Model name for attendance
+            'create', // Method to create a new record
+            [
+              {
+                employee_id: employeeId, // Employee ID
+                check_in: checkIn, // Check-in time (string in format: 'YYYY-MM-DD HH:MM:SS')
+                check_out: checkOut, // Check-out time (string in format: 'YYYY-MM-DD HH:MM:SS')
+              },
+            ],
+          ],
+          (err: any, record: any) => {
+            if (err) {
+              reject(err); // Reject if there's an error
+            } else {
+              resolve(record); // Resolve with the created attendance record
+            }
+          },
+        );
+      });
+
+      return record; // Return the created attendance record
+    } catch (err) {
+      console.error('Error creating Odoo attendance:', err);
+    }
+    return {}; // Return empty object in case of error
+  };
+
+  getEmployees = async (uid: number): Promise<any[]> => {
+    try {
+      const records = await new Promise((resolve, reject) => {
+        this.modelsClient.methodCall(
+          'execute_kw',
+          [
+            this.db,
+            uid,
+            this.password,
+            'hr.employee',
+            'search_read',
+            [],
+            { fields: ['id', 'resource_id', 'display_name'] },
+          ],
+          (err: any, records: any[]) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(records);
+            }
+          },
+        );
+      });
+      return records as any[];
+    } catch (err) {
+      console.error('Error getting Odoo employees:', err);
+      return [];
+    }
+  };
 }
